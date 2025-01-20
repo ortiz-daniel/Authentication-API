6483208821e7922ebabc3c8d69703f60104f2553
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-20 17:28:46 -0600

    add authentication router to main application

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
 from src.api.users import router as users_router
+from src.api.auth import router as auth_router
 from sqlmodel import SQLModel
 from src.database.models import engine
 
 app = FastAPI()
 app.include_router(users_router)
+app.include_router(auth_router)
 
 @app.on_event("startup")
 def create_tables():
