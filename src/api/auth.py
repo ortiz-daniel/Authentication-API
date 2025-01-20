e8134e5778951839bcbc20b1f89cabdb2971bace
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-20 17:28:37 -0600

    add login endpoint for user authentication with token generation

---
+++ src/api/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+from src.schemas.users import CreateUserSchema
+from src.database.crud import get_user_by_email
+from src.utils.token import Token
+
+router: APIRouter = APIRouter(prefix='/auth', tags=["Users"])  
+
+@router.post('/login')
+def login(body: CreateUserSchema):
+    """
+    Servicio que permite la autenticación de usuarios en la API.
+
+    El servicio recibe un JSON con los datos del usuario a autenticar y retorna un mensaje de éxito.
+
+    Returns:
+        JSON: Mensaje de éxito y token de autenticación con expiracion de 2 minutos a partir de la fecha de creación.
+    """
+    
+    body_to_dict: dict = body.dict()
+
+    user: User = get_user_by_email(body_to_dict['email'])
+
+    if user is None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={'message': 'Usuario no encontrado'}
+            )
+    else:
+        if user.password == body_to_dict['password']:
+            token: str = Token.encode_token(body_to_dict)
+            response: dict = {
+                'message': 'Usuario autenticado con éxito',
+                'token': token
+                }
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=response
+                )
+        else:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={'message': 'Contraseña incorrecta'}
+                )
