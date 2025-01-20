ebd6c18eda84781deac2f80a43af32fe7e6588d4
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-20 12:28:11 -0600

    add get_user_by_email function to retrieve user by email

--- src/database/crud.py
+++ src/database/crud.py
@@ -11,3 +11,8 @@ def insert_user(user: dict) -> User:
         session.refresh(user)
 
     return user
+
+def get_user_by_email(email: str) -> User:
+    with Session(engine) as session:
+        user = session.query(User).filter(User.email == email).first()
+    return user
