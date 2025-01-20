f5a03e9ff28c8eff0c199554b08c63833325a4b6
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-20 11:21:44 -0600

    add database table creation on startup event

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 
 from src.api.users import router as users_router
 from sqlmodel import SQLModel
+from src.database.models import engine
 
 app = FastAPI()
 app.include_router(users_router)
+
+@app.on_event("startup")
+def create_tables():
+    SQLModel.metadata.create_all(engine)
 
 @app.get('/')
 async def main():
