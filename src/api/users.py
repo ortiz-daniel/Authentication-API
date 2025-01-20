fdda1d4f5168589cb4c581bc07a9772201288729
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-20 12:28:41 -0600

    add login endpoint for user authentication with token generation

--- src/api/users.py
+++ src/api/users.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, status, Header
+from fastapi import APIRouter, status, Header, Depends
+from fastapi.security import HTTPBearer
 from fastapi.responses import JSONResponse
+from typing import Annotated
 
 from src.schemas.users import CreateUserSchema
-from src.database.crud import insert_user
+from src.database.crud import insert_user, get_user_by_email
 from src.database.models import User
 from src.utils.token import Token
 
@@ -32,3 +34,39 @@ async def create_user(body: CreateUserSchema):
         'token': token
         }
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=response)
+
+
+@router.post('/login')
+def login(body: CreateUserSchema):
+    """
+    Función que permite la autenticación de usuarios en la aplicación.
+
+    Returns:
+        JSON: Mensaje de éxito y token de autenticación con expiracion de 2 minutos a partir de la fecha de creación.
+    """
+    
+    body_to_dict: dict = body.dict()
+
+    user: User = get_user_by_email(body_to_dict['email'])
+
+    if user is None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={'message': 'Usuario no encontrado'}
+            )
+    else:
+        if user.password == body_to_dict['password']:
+            token: str = Token.encode_token(body_to_dict)
+            response: dict = {
+                'message': 'Usuario autenticado con éxito',
+                'token': token
+                }
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=response
+                )
+        else:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={'message': 'Contraseña incorrecta'}
+                )
