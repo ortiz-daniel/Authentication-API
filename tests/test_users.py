885a95416f20afc17349d13baadaebba8f6bf792
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-21 13:15:06 -0600

    add tests for user registration, login, and login history functionality

---
+++ tests/test_users.py
@@ -0,0 +1,92 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+from src.database.models import engine, User, LoginSessions
+
+from sqlmodel import SQLModel
+
+client = TestClient(app)
+
+def test_register_user():
+    import uuid
+
+    SQLModel.metadata.create_all(engine)
+
+    response = client.post(
+        '/users/',
+        json={
+            'email': f'{str(uuid.uuid4())}@test.com',
+            'password': 'mockpassword'
+        }
+    )
+    assert response.status_code == 201
+    assert response.json()['message'] == 'Usuario creado con éxito'
+
+def test_login_user():
+    import uuid
+
+    SQLModel.metadata.create_all(engine)
+
+    email = f'{str(uuid.uuid4())}@test.com'
+    password = 'mockpassword'
+
+    response = client.post(
+        '/users/',
+        json={
+            'email': email,
+            'password': password
+        }
+    )
+    assert response.status_code == 201
+
+    response = client.post(
+        '/auth/login',
+        json={
+            'email': email,
+            'password': password
+        }
+    )
+
+    assert response.status_code == 200
+    assert 'token' in response.json()
+
+
+def test_login_history():
+    import uuid
+
+    SQLModel.metadata.create_all(engine)
+
+    email = f'{str(uuid.uuid4())}@test.com'
+    password = 'mockpassword'
+
+    response = client.post(
+        '/users/',
+        json={
+            'email': email,
+            'password': password
+        }
+    )
+    assert response.status_code == 201
+
+    response = client.post(
+        '/auth/login',
+        json={
+            'email': email,
+            'password': password
+        }
+    )
+
+    assert response.status_code == 200
+    assert 'token' in response.json()
+
+    token = response.json()['token']
+
+    response = client.get(
+        '/users/sessions',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == 200
+    assert 'history_sessions' in response.json()
+    assert len(response.json()['history_sessions']) == 1
