3e731fc429061205f05f28d2ba1687449d009ded
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-21 00:43:05 -0600

    add login session management functions and improve user retrieval methods

--- src/database/crud.py
+++ src/database/crud.py
@@ -1,8 +1,16 @@
-from src.database.models import User, engine
+from src.database.models import User, engine, LoginSessions
+from typing import List
 
 from sqlmodel import Session
 
 def insert_user(user: dict) -> User:
+    """
+    Args:
+        user (dict): Recibe un diccionario con los datos del usuario a crear.
+
+    Returns:
+        User: Devuelve el usuario creado en la base de datos.
+    """
     user = User(**user)
 
     with Session(engine) as session:
@@ -13,6 +21,53 @@ def insert_user(user: dict) -> User:
     return user
 
 def get_user_by_email(email: str) -> User:
+    """
+    Args:
+        email (str): Recibe el email del usuario a buscar.
+
+    Returns:
+        User: Devuelve el usuario encontrado en la base de datos, si no lo encuentra devuelve None.
+    """
     with Session(engine) as session:
         user = session.query(User).filter(User.email == email).first()
     return user
+
+def get_logins_by_user(email: str) -> List[LoginSessions]:
+    """
+    Args:
+        email (str): Email del usuario a buscar.
+
+    Returns:
+        List[LoginSessions]: Devuelve una lista con las sesiones de login del usuario.
+    """
+
+    user = get_user_by_email(email)
+
+    user_id = user.id
+
+    with Session(engine) as session:
+        logins = session.query(LoginSessions).filter(LoginSessions.user_id == user_id).all()
+
+    return logins
+
+
+def insert_login_session(email: str, success: bool) -> LoginSessions:
+    """
+    Args:
+        user_email (str): Email del usuario.
+        success (bool): Indica si el login fue exitoso o no.
+
+    Returns:
+        LoginSessions: Devuelve la sesión de login creada en la base de datos.
+    """
+
+    user = get_user_by_email(user_email)
+
+    login = LoginSessions(user=user, success=success)
+
+    with Session(engine) as session:
+        session.add(login)
+        session.commit()
+        session.refresh(login)
+
+    return login
