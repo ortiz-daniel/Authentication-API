9d3eb3b3e29aceaf178a7ef0c2d20e2b28c25f3c
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-21 00:44:31 -0600

    add login sessions model and establish relationship with user

--- src/database/models.py
+++ src/database/models.py
@@ -1,11 +1,20 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Relationship
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     email: str = Field(max_length=64, unique=True)
     password: str = Field(max_length=64)
     is_active: bool = Field(default=True)
+    
+    login_sessions: list["LoginSessions"] = Relationship(back_populates="user")
+    
 
+class LoginSessions(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    success: bool = Field(default=True)
+    
+    user_id: int = Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="login_sessions")
 
 url: str = "sqlite:///database.db"
 engine = create_engine(url=url, echo=True)
