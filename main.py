b32169d2e7d3f05826c6aa9a3667c62c04495a6a
Daniel-Ortiz1210 <thisisdanielortiz@gmail.com>
2025-01-21 11:32:25 -0600

    add entry point to run the application with Uvicorn

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ def create_tables():
 @app.get('/')
 async def main():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
